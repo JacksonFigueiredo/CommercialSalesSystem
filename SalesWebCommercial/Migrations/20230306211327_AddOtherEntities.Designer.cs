5371142330dfb620958625d2b300f6552fb1a9a4
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-03-06 18:13:34 -0300

    Including Migrations

--- SalesWebCommercial/Migrations/20230306211327_AddOtherEntities.Designer.cs
+++ SalesWebCommercial/Migrations/20230306211327_AddOtherEntities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace SalesWebCommercial.Migrations
 {
     [DbContext(typeof(SalesWebCommercialContext))]
-    partial class SalesWebCommercialContextModelSnapshot : ModelSnapshot
+    [Migration("20230306211327_AddOtherEntities")]
+    partial class AddOtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
