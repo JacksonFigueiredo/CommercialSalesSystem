ecbf33d1806d26acba8c0d5629fa1941323f2bef
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-03-06 18:06:26 -0300

    Adding Operations.

--- SalesWebCommercial/Models/Department.cs
+++ SalesWebCommercial/Models/Department.cs
@@ -4,5 +4,18 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
+
 }
