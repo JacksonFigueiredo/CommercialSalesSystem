2b57ab7f80965c8544ae81a25c03fc70d23fd5e9
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-03-10 14:22:19 -0300

    Pushing Layout

---
+++ SalesWebCommercial/Controllers/SalesRecordsController.cs
@@ -0,0 +1,22 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebCommercial.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }       
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }       
+        public IActionResult GroupedSearch()
+        {
+            return View();
+        }
+
+
+    }
+}
